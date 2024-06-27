e71203704cfd0b3672969a072e32628c7cc2c2a5
F.K.A. Soffers <diorcula@duck.com>
2024-06-27 17:16:23 +0200

    working on Declaration, checking expressions

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,21 +1,59 @@
 package nl.han.ica.icss.checker;
 
-import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.selectors.TagSelector;
 import nl.han.ica.icss.ast.types.ExpressionType;
-
-import java.util.HashMap;
-
 
 
 public class Checker {
 
-    private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+//    private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-        // variableTypes = new HANLinkedList<>();
+//         variableTypes = new HANLinkedList<>();
+        System.out.println("----------- root" + ast.root);
+        checkStyleSheet(ast.root);
+    }
+
+    private void checkStyleSheet(Stylesheet stylesheet) {
+        System.out.println("checking stylesheet");
+        System.out.println(stylesheet.getChildren());
+
+        for (ASTNode child : stylesheet.getChildren()) {
+            System.out.println("checking children in stylesheet" + child.toString());
+            checkStyleRule(child);
+        }
+    }
+
+    private void checkStyleRule(ASTNode astNode) {
+        for (ASTNode child : astNode.getChildren()) {
+            if (child instanceof Selector) {
+                System.out.println("checking selectorrrrrrrrr");
+                checkSelector(child);
+            }
+        }
+    }
+
+    private void checkSelector(ASTNode astNode) {
+        for (ASTNode child : astNode.getChildren()) {
+//            if (child instanceof TagSelector) {
+                System.out.println("--------checking selector tag");
+                checkDeclaration(child);
+//            }
+        }
+    }
+
+    private void checkDeclaration(ASTNode astNode) {
+        // setError als ze niet matchen
+        System.out.println("---- checking declaration");
+//        Declaration declaration = (Declaration) astNode;
+//        Expression expression = declaration.expression;
+//        System.out.println(declaration.property.name + expression);
+//        ExpressionType expressionType = checkExpression(declaration.expression);
 
     }
 
-    
+//    private ExpressionType checkExpression(Expression expression) {
+//        //
+//    }
 }
